d3c92a424dff4f2d1345250a71aa5ffb0d57087b
PanDaWangLuoKeJi_sunshine_guo <sunshine.guo@pdabc.com>
2019-06-18 17:42:14 +0800

    日期工具类

--- src/main/java/com/tsyj/utils/CollectionUtils.java
+++ src/main/java/com/tsyj/utils/CollectionUtils.java
@@ -11,16 +11,8 @@ import java.util.*;
  */
 public class CollectionUtils extends org.springframework.util.CollectionUtils {
 
-    public static <S, T> List<T> transfer(Collection<S> source) {
-        ArrayList targetList = new ArrayList();
-        if (source == null) return null;
-        source.forEach(s -> targetList.add(s));
-        return targetList;
-    }
-
-
     public static void main(String[] args) {
-        String key = "root/";
+      /*  String key = "root/";
         List<String> list = new ArrayList();
         list.add(key + "test1");
         list.add(key + "test2");
@@ -28,7 +20,25 @@ public class CollectionUtils extends org.springframework.util.CollectionUtils {
         list.add(key + "test2");
         int index = Collections.binarySearch(list, key + "test2");
         System.out.println(index);
-        System.out.println(list.get(index - 1));
+        System.out.println(list.get(index - 1));*/
+
+        List<Integer> ids = Lists.newArrayList(1, 2, 3, 4, 5, 6);
+        List<List<Integer>> allList =splitList(ids,5);
+        if(CollectionUtils.isEmpty(allList)){
+            return;
+        }
+        allList.forEach(a->{
+            a.forEach(b->System.out.println(b));
+            System.out.println("*********************");
+        });
+    }
+
+
+    public static <S, T> List<T> transfer(Collection<S> source) {
+        ArrayList targetList = new ArrayList();
+        if (source == null) return null;
+        source.forEach(s -> targetList.add(s));
+        return targetList;
     }
 
 
@@ -61,10 +71,35 @@ public class CollectionUtils extends org.springframework.util.CollectionUtils {
     }
 
 
-    private static <T> List<T> batchList(List<T> list){
+    private static <T> List<T> batchList(List<T> list) {
         if (CollectionUtils.isEmpty(list)) {
             return Lists.newArrayList();
         }
         return null;
+    }
+
+
+    /**
+     * 按指定大小，分隔集合，将集合按规定个数分为n个部分
+     *
+     * @param list
+     * @param len
+     * @return
+     * @author guos
+     * @date 2019/6/18 17:35
+     **/
+    public static <T> List<List<T>> splitList(List<T> list, int len) {
+        if (CollectionUtils.isEmpty(list) || len < 1) {
+            return null;
+        }
+        List<List<T>> result = Lists.newArrayList();
+        int size = list.size();
+        int count = (size + len - 1) / len;
+
+        for (int i = 0; i < count; i++) {
+            List<T> subList = list.subList(i * len, ((i + 1) * len > size ? size : len * (i + 1)));
+            result.add(subList);
+        }
+        return result;
     }
 }
