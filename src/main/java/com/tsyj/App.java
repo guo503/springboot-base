ca68affbd99e111eeefd0a611a599ed4fd1bfdc2
guos <sunshine.guo@pdabc.com>
2019-10-28 20:13:53 +0800

    mapper优化

--- src/main/java/com/tsyj/App.java
+++ src/main/java/com/tsyj/App.java
@@ -3,11 +3,10 @@ package com.tsyj;
 import mybatis.spring.annotation.ExtMapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.tsyj")
+//@ComponentScan(basePackages = "com.tsyj")
 @ExtMapperScan({"com.tsyj.mapper"})
 @EnableScheduling
 public class App {
